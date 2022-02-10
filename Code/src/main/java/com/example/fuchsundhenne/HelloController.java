eabd5d7d5d829396d4079eee37faecbca498358a
Sanna0908 <sarahanna0908@gmx.at>
2022-02-10 11:53:52 +0100

    hello

--- Code/src/main/java/com/example/fuchsundhenne/HelloController.java
+++ Code/src/main/java/com/example/fuchsundhenne/HelloController.java
@@ -3,7 +3,7 @@ package com.example.fuchsundhenne;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class HelloController{
     @FXML
     private Label welcomeText;
 
