422eceae2458f025b7c492549179ae54eb964776
ShizoFX <alessandro.davare@htl-saalfelden.at>
2022-02-10 12:09:43 +0100

    grias di add

--- Code/src/main/java/com/example/fuchsundhenne/HelloController.java
+++ Code/src/main/java/com/example/fuchsundhenne/HelloController.java
@@ -6,6 +6,7 @@ import javafx.scene.control.Label;
 public class HelloController {
     @FXML
     private Label welcomeText;
+    private Label griasdi;
 
     @FXML
     protected void onHelloButtonClick() {
