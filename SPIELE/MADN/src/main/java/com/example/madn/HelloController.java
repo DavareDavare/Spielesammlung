6e7e63b12a98f88867f1cbc2e7a929f90310546d
Sanna0908 <sarahanna0908@gmx.at>
2022-02-24 15:02:16 +0100

    Madn

--- SPIELE/MADN/src/main/java/com/example/madn/HelloController.java
+++ SPIELE/MADN/src/main/java/com/example/madn/HelloController.java
@@ -16,29 +16,63 @@ public class HelloController {
 
 
     public void initialize(){
-        for(int i=0; i<11; i++){
-            for(int j=0; j<11; j++){
-
-                if(i==0||i==1||i==2||i==3||i==7||i==8||i==9||i==10){
-                    if(j==0||j==1||j==2||j==3||j==7||j==8||j==9||j==10){
-                        feld[i][j] = 'O';
+        for(int i=0; i<11; i++) {
+            for (int j = 0; j < 11; j++) {
+                /*
+                 if (i == 0 || i == 1 || i == 2 || i == 3 || i == 7 || i == 8 || i == 9 || i == 10) {
+                    if (j == 0 || j == 1 || j == 2 || j == 3 || j == 7 || j == 8 || j == 9 || j == 10) {
+                        feld[i][j] = ' ';
                         System.out.print(feld[i][j] + "  ");
-                    }
-                    else{
+                    } else {
                         feld[i][j] = 'X';
                         System.out.print(feld[i][j] + "  ");
                     }
                 }
-                else if(i==5&&j==5){
-                    feld[i][j] = 'O';
+
+                else if (i == 5 && j == 5) {
+                    feld[i][j] = ' ';
                     System.out.print(feld[i][j] + "  ");
+                } else {
+                    feld[i][j] = 'X';
+                    System.out.print(feld[i][j] + "  ");
+                }*/
+
+                if(i==0||i==3||i==7||i==10){
+                    if(j==4||j==5||j==6){
+                        feld[i][j] = 'X';
+                        System.out.print(feld[i][j] + "  ");
+                    }
+                    else {
+                        feld[i][j] = ' ';
+                        System.out.print(feld[i][j] + "  ");
+                    }
                 }
-                else{
+                if(i==1||i==2||i==8||i==9){
+                    if(j==1||j==2||j==8||i==9){
+                        feld[i][j] = 'X';
+                        System.out.print(feld[i][j] + "  ");
+                    }
+                    else {
+                        feld[i][j] = ' ';
+                        System.out.print(feld[i][j] + "  ");
+                    }
+                }
+                if(i==4||i==5||i==6){
                     feld[i][j] = 'X';
                     System.out.print(feld[i][j] + "  ");
                 }
+                if(i==5&&j==5){
+                    feld[i][j] = ' ';
+                    System.out.print(feld[i][j] + "  ");
+                }
+
+
+
+
             }
             System.out.println(" ");
         }
+
     }
+
 }
