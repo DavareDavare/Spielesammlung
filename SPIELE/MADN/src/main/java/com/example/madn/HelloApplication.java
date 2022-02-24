0f0006551fd27e947e65b509b33bf8af446cce8a
Sanna0908 <sarahanna0908@gmx.at>
2022-02-24 13:21:27 +0100

    Madn

--- SPIELE/MADN/src/main/java/com/example/madn/HelloApplication.java
+++ SPIELE/MADN/src/main/java/com/example/madn/HelloApplication.java
@@ -2,12 +2,14 @@ package com.example.madn;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class HelloApplication extends Application {
+public class HelloApplication extends Application implements Initializable {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
@@ -20,5 +22,10 @@ public class HelloApplication extends Application {
 
     public static void main(String[] args) {
         launch();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
 }
